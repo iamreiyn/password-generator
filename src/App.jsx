b4f3cf982de41c1c10309bd864b9b6c9269b15a5
renisal <98770205+renisal@users.noreply.github.com>
2022-05-15 16:33:42 +0530

    Initial commit/unfinished work

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Structure from "./components/Structure";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Structure/>
+    </>
+  );
+}
+
+export default App;
